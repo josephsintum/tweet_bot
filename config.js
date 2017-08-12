b1900525d0cb3d3b0491dd27fdeddab377788001
Joseph Sintum <sjumbam15@gmail.com>
2017-08-12 14:20:24 +0100

    update config.js
    How to get key and token

--- config.js
+++ config.js
@@ -4,3 +4,12 @@ module.exports = {
   access_token: 'getyourkeyfromtwitteraccount',
   access_token_secret: 'getyourkeyfromtwitteraccount'
 };
+ 
+/* 
+To get this keys 
+
+go to https://apps.twitter.com 
+create an app
+fill the required information and submit
+then you'll get the keys and token
+*/
